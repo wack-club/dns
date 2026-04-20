e8ae319ab62c2011a4d824de3da2bdb8de6b4ba0
Ingo Wolf <me@ingo.au>
2026-04-20 16:31:37 +1000

    Update dnsconfig.js

--- dnsconfig.js
+++ dnsconfig.js
@@ -12,5 +12,5 @@ D("wack.club", REG_NONE, DnsProvider(DSP_CLOUDFLARE),
 	CNAME("purelymail3._domainkey", "key3.dkimroot.purelymail.com."),
 	MX("@", 69, "mailserver.purelymail.com."),
 	TXT("@", "v=spf1 include:_spf.purelymail.com ~all"),
-	TXT("@", "purelymail_ownership_proof=cfccc9e7f9c8bf6bb31c87ca1c5339dcbe3bdf03b07de22c72e1229137f45e514651bbb5432851eedcf883f3a8542eeccc2d3b9341d3c5b36f461e0cfabc5758"),
+	TXT("@", "purelymail_ownership_proof=cfccc9e7f9c8bf6bb31c87ca1c5339dcbe3bdf03b07de22c72e1229137f45e514651bbb5432851eedcf883f3a8542eeccc2d3b9341d3c5b36f461e0cfabc5758")
 );
