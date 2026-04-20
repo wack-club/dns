447efca9c6552b3cb8b8f4f63cd680ccf89da5c4
Ingo Wolf <me@ingo.au>
2026-04-20 05:43:33 +0000

    change provider to cloudflare

--- dnsconfig.js
+++ dnsconfig.js
@@ -2,8 +2,8 @@
 /// <reference path="types-dnscontrol.d.ts" />
 
 var REG_NONE = NewRegistrar("none");
-var DNS_BIND = NewDnsProvider("bind");
+var DSP_CLOUDFLARE = NewDnsProvider("cloudflare");
 
-D("wack.club", REG_NONE, DnsProvider(DNS_BIND),
+D("wack.club", REG_NONE, DnsProvider(DSP_CLOUDFLARE),
     A("@", "1.2.3.4"),    // "@" means the domain's apex.
 );
