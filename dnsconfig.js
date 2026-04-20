944e8e7cf5543421f022e2b592dd7426190c7301
Ingo Wolf <me@ingo.au>
2026-04-20 16:35:58 +1000

    remove trailing comma

--- dnsconfig.js
+++ dnsconfig.js
@@ -17,6 +17,6 @@ D(
   TXT("@", '"v=spf1 include:_spf.purelymail.com ~all"'),
   TXT(
     "@",
-    '"purelymail_ownership_proof=cfccc9e7f9c8bf6bb31c87ca1c5339dcbe3bdf03b07de22c72e1229137f45e514651bbb5432851eedcf883f3a8542eeccc2d3b9341d3c5b36f461e0cfabc5758"',
-  ),
+    '"purelymail_ownership_proof=cfccc9e7f9c8bf6bb31c87ca1c5339dcbe3bdf03b07de22c72e1229137f45e514651bbb5432851eedcf883f3a8542eeccc2d3b9341d3c5b36f461e0cfabc5758"'
+  )
 );
